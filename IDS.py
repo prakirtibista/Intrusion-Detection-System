73d3174b38349121fe979aee59731434efc7a977
prakirtibista <158180233+prakirtibista@users.noreply.github.com>
2024-10-20 14:39:00 +0530

    Create IDS.py

---
+++ IDS.py
@@ -0,0 +1,27 @@
+import cv2
+from cvzone.PoseModule import PoseDetector
+import send
+detector=PoseDetector()
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+l=[]
+flag = True
+while True: 
+    success,img=cap.read()
+    img=detector.findPose(img)
+    imlist,bbox = detector.findPosition(img)
+    #print(imlist)
+    if len(imlist)>0:
+        print("Human Detected")
+        l.append(1)
+    if len(l)>50 and flag:
+        flag=False
+        send.sendSms()
+    cv2.imshow("output",img)
+    q=cv2.waitKey(1)
+    if q==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
